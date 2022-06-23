be80f5205ee3c42e2c82ea210ba30c45a2919abd
Rycolopot <57956865+Rycolopot@users.noreply.github.com>
2022-06-24 03:22:35 +0700

    Create p.sh

--- p.sh
+++ p.sh
@@ -4,32 +4,16 @@
 #
 # Distributed under terms of the MIT license.
 #
-echo "hello $nama"
-echo -n "masukkan user linux anda : "
+
+
+echo "Jangan Lupa di bantu klik subscribe 😅"
+echo "Twitter  : @rehan_ssf"
+echo "Telegram : @paperhang"
+
+echo -n "seperti biasa ketik yes untuk update/upgrade : "
 read user
 
-echo "install maven"
-mkdir -p /home/$user/programming/build-tool/apache-maven
-wget http://mirror.wanxp.id/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
-tar -xvzf apache-maven-3.3.9-bin.tar.gz
-rm apache-maven-3.3.9-bin.tar.gz
-mv apache-maven-3.3.9/* /home/$user/programming/build-tool/apache-maven/
-rmdir apache-maven-3.3.9/
 
-echo "membuat file environment"
-touch environment
 
-echo "membuat path maven"
-echo "M2_HOME=/home/$user/programming/build-tool/apache-maven" | tee -a /home/$user/environment
-
-echo "copy file environment"
-sudo cp environment /etc/environment
-
-echo "update aplikasi"
-sudo apt-get update
-
-echo "upgrade aplikasi"
-sudo apt-get upgrade -y
-
-echo "dist upgrade aplikasi"
-sudo apt-get dist-upgrade -y
+sudo apt update && sudo apt upgrade -y
+sudo apt install curl build-essential git wget jq make gcc tmux -y
