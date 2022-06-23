62639f80a0eaa8f890c49477ad8286e52de873cc
Rycolopot <57956865+Rycolopot@users.noreply.github.com>
2022-06-24 02:44:53 +0700

    Add files via upload

---
+++ p.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# Copyright (C) 2022 Rycolopot <rycolopot@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install curl build-essential git wget jq make gcc tmux -y
+
+ver="1.18.2"
+cd $HOME
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+source ~/.bash_profile
+go version
