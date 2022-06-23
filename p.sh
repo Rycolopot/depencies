0c48da2a5c282a7bdb2ff1e2e5a460a72d6d5736
Rycolopot <57956865+Rycolopot@users.noreply.github.com>
2022-06-24 02:52:41 +0700

    Update p.sh

--- p.sh
+++ p.sh
@@ -9,11 +9,13 @@ sudo apt update && sudo apt upgrade -y
 
 sudo apt install curl build-essential git wget jq make gcc tmux -y
 
+
+echo "install GO"
 ver="1.18.2"
 cd $HOME
 wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm -rf /usr/local/go
+tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
 rm "go$ver.linux-amd64.tar.gz"
 echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
 source ~/.bash_profile
