2251ff81f271c78a21849e084a8bd0ce5c3b0574
Rycolopot <57956865+Rycolopot@users.noreply.github.com>
2022-06-24 05:12:16 +0700

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -15,9 +15,15 @@ echo " seperti biasa di update dulu ya bang biar gak eror nanti "
 echo -n " klik enter aja bang 😂 !"
 read user
 echo
+echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
 sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
 sudo apt install curl build-essential git wget jq make gcc tmux -y
 echo
+
+
+echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
 cd $HOME && rm $HOME/sei-chain -rf
 git clone https://github.com/sei-protocol/sei-chain.git && cd $HOME/sei-chain
 git checkout 1.0.4beta
